9a8ee096893488a16d72017a46d8f934c3feca39
Mohamed Ahmed <41699093+Mo-Ali98@users.noreply.github.com>
2021-10-07 19:30:35 +0100

    feat(packages): prettier

--- src/app.ts
+++ src/app.ts
@@ -1,47 +1,51 @@
-import express, { Application, Request, Response, NextFunction, response } from 'express';
-import * as cheerio from 'cheerio';
-import axios from 'axios';
-
-
+import express, {
+  Application,
+  Request,
+  Response,
+  NextFunction
+} from "express";
+import * as cheerio from "cheerio";
+import axios from "axios";
 
 const app: Application = express();
 const port = 5000;
 
 interface Articles {
-    title: string;
-    url?: string;
+  title: string;
+  url?: string;
 }
 
 const getData = async (): Promise<Articles[]> => {
-    const url = "https://www.theguardian.com/uk";
-    const articles: Articles[] = [];
+  const url = "https://www.theguardian.com/uk";
+  const articles: Articles[] = [];
 
-    try {
-        const { data } = await axios.get(url);
-        const $ = cheerio.load(data);
+  try {
+    const { data } = await axios.get(url);
+    const $ = cheerio.load(data);
 
-        $('.fc-item__title', data).each((i, element) => {
-            const title = $(element).text();
-            const url = $(element).find('a').attr('href');
-            articles.push({ title, url })
-        })
+    $(".fc-item__title", data).each((i, element) => {
+      const title = $(element).text();
+      const url = $(element).find("a").attr("href");
+      articles.push({ title, url });
+    });
 
-        return articles
-    } catch (err) {
-        console.error(err);
-        return []
-    }
+    return articles;
+  } catch (err) {
+    console.error(err);
+    return [];
+  }
+};
 
+app.get("/", async (req: Request, res: Response, next: NextFunction) => {
+  let results = await getData();
+  let tags: string = "";
+  results.map((r) => {
+    tags += `<h3>${r.title}</h3> <p>${r.url}</p>`;
+  });
 
-
-}
-
-app.get('/', async (req: Request, res: Response, next: NextFunction) => {
-
-    let results = await getData();
-    res.send(`<h3>${results[0].title}</h3> <h4>${results[0].url}</h4>`);
+  res.send(tags);
 });
 
 app.listen(port, () => {
-    console.log(`server started at http://localhost:${port}`);
+  console.log(`server started at http://localhost:${port}`);
 });
